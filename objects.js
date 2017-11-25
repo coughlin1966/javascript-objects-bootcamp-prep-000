f7b04dd20e4b9de76189e4db828e3f2b722c3fc1
Peter Coughlin <coughlin1966@gmail.com>
2017-11-25 23:55:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,9 +5,16 @@ function updatePlaylist (list,artist,song) {
 }
 updatePlaylist(playlist,ACDC,'Dynamite')
 
+<<<<<<< HEAD
 function removeFromPlaylist (list,artist) {
 var newList = Object.assign({},list)
 delete newList[artist]
   return newList
 }
 removeFromPlaylist(playlist,Zepplin)
+=======
+function removeFromPlaylist (list,song) {
+  return delete list.artist
+}
+removeFromPlaylist(playlist,Zappa)
+>>>>>>> bb9a1800250c35a82b062fcc45cbae34022cea07
